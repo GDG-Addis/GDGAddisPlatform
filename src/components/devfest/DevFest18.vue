5a54e844060f1f9cd62063f5aafd9bf04d3c7f68
nabiloo19 <nabil.alanssi19@gmail.com>
2019-08-31 14:18:31 +0300

    Photos Optimized

--- src/components/devfest/DevFest18.vue
+++ src/components/devfest/DevFest18.vue
@@ -1,28 +1,7 @@
 <template>
   <v-layout style="background-color:#424242;">
-    <v-layout wrap align-center justify-center row style="background-color:#444444;">
-      <v-carousel
-        width="100%"
-        height="100%"
-        show-arrows-on-hover
-        hide-delimiters
-        interval="2500"
-        tile
-        transition="transition">
-        <v-carousel-item v-for="(slider,k) in sliders" :key="k">
-          <img
-            :src="require(`@/assets/img/devfest/sliders/${slider.url}`)"
-            width="100%"
-            height="100%"
-          />
-        </v-carousel-item>
-      </v-carousel>
-    </v-layout>
-
-<grid-list-xl></grid-list-xl>
-
+    
   </v-layout>
-
 </template>
 
 <script>
