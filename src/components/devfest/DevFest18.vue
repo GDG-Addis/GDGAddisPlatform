0759179a53e624a580812618b96603649ad12bd5
nabiloo19 <nabil.alanssi19@gmail.com>
2019-09-03 12:19:41 +0300

    Grid-Layout Updated

--- src/components/devfest/DevFest18.vue
+++ src/components/devfest/DevFest18.vue
@@ -1,26 +1,42 @@
 <template>
-  <v-container class="m5">
+  <v-container>
     <v-layout row wrap>
-      <v-flex xs12 md2>
+      <v-flex xs4 md2>
         <v-btn outline block color="primary">1</v-btn>
       </v-flex>
+
       <v-flex xs4 md2>
-        <v-btn outline block color="primary">2</v-btn>
-      </v-flex>
-      <v-flex xs4 md2>
-        <v-btn outline block color="primary">2</v-btn>
-      </v-flex>
-      <v-flex xs4 md2>
-        <v-btn outline block color="primary">2</v-btn>
+        <v-btn outline block color="primary">11</v-btn>
       </v-flex>
     </v-layout>
 
-    <v-layout row wrap justify-center>
-      <v-flex xs4 md3>
-        <v-btn outline block color="#fff">2</v-btn>
+    <v-layout justify-end>
+      <v-flex xs4 md2>
+        <v-btn outline block color="primary">12</v-btn>
       </v-flex>
-      <v-flex xs4 md3>
-        <v-btn outline block color="success">2</v-btn>
+    </v-layout>
+
+    <v-layout row wrap>
+      <v-layout column wrap>
+        <v-flex xs4 md2>
+          <v-btn outline block color="primary">3</v-btn>
+        </v-flex>
+      </v-layout>
+    </v-layout>
+    
+<v-layout row wrap>
+     <v-flex xs4 md2>
+        <v-btn outline block color="primary">2</v-btn>
+      </v-flex>
+</v-layout>
+    <v-layout row wrap>
+      <v-layout column wrap>
+        <v-flex xs4 md2>
+          <v-btn outline block color="primary">8</v-btn>
+        </v-flex>
+      </v-layout>
+      <v-flex xs4 md2>
+        <v-btn outline block color="primary">9</v-btn>
       </v-flex>
     </v-layout>
   </v-container>
