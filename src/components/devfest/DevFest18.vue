30ecd4a587eefc26c85ad665c38c435e8588bd45
nabiloo19 <nabil.alanssi19@gmail.com>
2019-09-04 08:28:59 +0300

    Updated

--- src/components/devfest/DevFest18.vue
+++ src/components/devfest/DevFest18.vue
@@ -1,62 +1,6 @@
 <template>
   <v-container>
-           
-    <v-layout row wrap>
-      <v-flex xs4 md2>
-        <v-img
-          :src="require(`@/assets/img/devfest/sliders/slider1.png`)"
-          height="100%"
-          width="100%"
-        ></v-img>
-      </v-flex>
 
-      <v-flex xs4 md2 >
-        <v-img
-          :src="require(`@/assets/img/devfest/sliders/slider2.png`)"
-          height="100%"
-          width="100%"
-           
-        ></v-img>
-      </v-flex>
-    </v-layout>
-
-    <v-layout justify-end>
-      <v-flex xs4 md2>
-        <v-img
-          :src="require(`@/assets/img/devfest/sliders/slider3.png`)"
-          height="100%"
-          width="100%"
-        ></v-img>
-      </v-flex>
-    </v-layout>
-
-    <v-layout row wrap>
-      <v-layout column wrap>
-        <v-flex xs4 md2>
-          <v-img
-            :src="require(`@/assets/img/devfest/sliders/slider4.png`)"
-            height="100%"
-            width="100%"
-          ></v-img>
-        </v-flex>
-      </v-layout>
-    </v-layout>
-
-    <v-layout row wrap>
-      <v-flex xs4 md2>
-        
-      </v-flex>
-    </v-layout>
-    <v-layout row wrap>
-      <v-layout column wrap>
-        <v-flex xs4 md2>
-          <v-btn outline block color="primary">8</v-btn>
-        </v-flex>
-      </v-layout>
-      <v-flex xs4 md2>
-        <v-btn outline block color="primary">9</v-btn>
-      </v-flex>
-    </v-layout>
   </v-container>
 </template>
 
