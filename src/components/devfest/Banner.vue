7a14f5aa04599b64a32512f81260b1aec2cacb2f
Melake Wubshet <wubshetmelake@gmail.com>
2019-09-04 11:56:44 +0300

    Sliders are updated

--- src/components/devfest/Banner.vue
+++ src/components/devfest/Banner.vue
@@ -1,8 +1,5 @@
 <template>
-  <v-layout style="background-color:#424242;">
-    <v-layout wrap align-center justify-center row style="background-color:#444444;">
       <v-carousel
-        width="100%"
         height="100%"
         show-arrows-on-hover
         hide-delimiters
@@ -11,15 +8,12 @@
         transition="transition"
       >
         <v-carousel-item v-for="(slider,k) in sliders" :key="k">
-          <img
+          <v-img
             :src="require(`@/assets/img/devfest/sliders/${slider.url}`)"
-            width="100%"
             height="100%"
           />
         </v-carousel-item>
       </v-carousel>
-    </v-layout>
-  </v-layout>
 </template>
 
 <script>
--- src/components/home/Slider.vue
+++ src/components/home/Slider.vue
@@ -1,23 +1,19 @@
 <template>
-  <v-layout style="background-color:#424242;">
-    <v-layout wrap align-center justify-center row style="background-color:#444444;">
       <v-carousel
-        width="100%"
-        height="100%"
         show-arrows-on-hover
         hide-delimiters
         interval="2500"
         tile
         transition="transition"
+        class="pa-0"
+        height="100%"
       >
         <v-flex>
-          <v-carousel-item v-for="(img, i) in images" :key="i">
-            <img :src="img" :lazy-src="img" width="100%" height="100%" />
+          <v-carousel-item v-for="(img, i) in images" :key="i" width="100%">
+            <v-img :src="img" :lazy-src="img" height="100%" />
           </v-carousel-item>
         </v-flex>
       </v-carousel>
-    </v-layout>
-  </v-layout>
 </template>
 
 <script>
